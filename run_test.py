518703613dff0a4beb4c9d477fecc4f5b57c0c84
linshumo <linshumo@163.com>
2020-03-10 22:30:29 +0800

    htmltestrunnernew

--- run_test.py
+++ run_test.py
@@ -20,14 +20,14 @@ suite = unittest.TestSuite()
 loader = unittest.TestLoader()
 suite.addTest(loader.loadTestsFromModule(test_main_stream))
 # 执行测试用例
-# runner = HTMLTestRunner(stream=open(os.path.join(report_dir,date+"report.html"),"wb"),
-#                         title="测试报告0225",
-#                         description="接口实战练习",
-#                         tester="Sunny")
-# runner.run(suite)
+runner = HTMLTestRunner(stream=open(os.path.join(report_dir,date+"report.html"),"wb"),
+                        title="测试报告0225",
+                        description="接口实战练习",
+                        tester="Sunny")
+runner.run(suite)
 
-br = BeautifulReport(suite)
-br.report("前程带项目用例",filename=date+"report.html",report_dir=report_dir)
+# br = BeautifulReport(suite)
+# br.report("前程带项目用例",filename=date+"report.html",report_dir=report_dir)
 
 
 # SendEmail.send_email("report.html","在测试套件中执行一遍发邮件")
